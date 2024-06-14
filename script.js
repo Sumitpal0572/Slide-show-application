7382f696bc7d66920832565ea925b8710c8617e8
Sumitpal0572 <asmyselfsumit@gmail.com>
2024-06-14 13:47:09 +0530

    final commit

---
+++ script.js
@@ -0,0 +1,19 @@
+
+const images = [
+    "https://images.unsplash.com/photo-1617635277889-df22350f81a0?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1616352116682-52d7941afd53?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1616824420717-20b194514953?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1609949242806-e53c8e502460?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+];
+
+let currentIndex = 0;
+let slideElement = document.getElementById("slideshow");
+
+//first image as default image
+slideElement.src = images[currentIndex];
+
+//slide-show
+setInterval(() => {
+    currentIndex = (currentIndex + 1) % images.length;
+    slideElement.src = images[currentIndex];
+}, 2000);
